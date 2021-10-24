5db354b11ced33f78e8ffda939dc3ba502d0f0af
Guillaume Gomez <guillaume.gomez@huawei.com>
2021-10-24 16:32:05 +0200

    Fix pylint lints

--- doc_aliases.py
+++ doc_aliases.py
@@ -1,8 +1,12 @@
+# pylint: disable=C0114,C0116,R1702,R0912,R0913,R0914,R0915,W0511,W0703
+
 import os
 import sys
 
 
-IGNORE_C_FNS = ["_get_reference_count", "_status", "_free", "_destroy", "_unref", "gdk_device_free_history"]
+IGNORE_C_FNS = [
+    "_get_reference_count", "_status", "_free", "_destroy", "_unref", "gdk_device_free_history",
+]
 IGNORE_FNS = [
     "deref", "deref_mut", "drop", "fmt", "clone", "to_value", "g_value_get_flags",
     "g_value_set_flags", "get_type",
@@ -10,30 +14,30 @@ IGNORE_FNS = [
 IGNORE_FNS_START = ["into_glib", "to_glib", "from_glib", "from_raw", "to_raw"]
 
 
-def is_valid_name(s):
-    for c in s:
-        if not c.isalnum() and c != '_':
+def is_valid_name(name):
+    for char in name:
+        if not char.isalnum() and char != '_':
             return False
-    for x in IGNORE_C_FNS:
-        if s.endswith(x):
+    for c_fn in IGNORE_C_FNS:
+        if name.endswith(c_fn):
             return False
-    for x in IGNORE_FNS_START:
-        if s.startswith(x):
+    for fn_start in IGNORE_FNS_START:
+        if name.startswith(fn_start):
             return False
-    if s in IGNORE_FNS:
+    if name in IGNORE_FNS:
         return False
     return True
 
 
-def get_fn_name(s):
-    parts = s.split(" ")
-    x = 0
-    while x < len(parts) and parts[x] != "fn":
-        x += 1
-    x += 1
-    if x >= len(parts):
+def get_fn_name(line):
+    parts = line.split(" ")
+    pos = 0
+    while pos < len(parts) and parts[pos] != "fn":
+        pos += 1
+    pos += 1
+    if pos >= len(parts):
         return None
-    return parts[x].split("<")[0].split("(")[0]
+    return parts[pos].split("<")[0].split("(")[0]
 
 
 # This to prevent to add the same doc alias more than once on a same function.
@@ -43,7 +47,7 @@ def need_doc_alias(content, pos, alias):
         stripped = content[pos].strip()
         if not stripped.startswith("#[") and not stripped.startswith("//"):
             return True
-        elif stripped == alias:
+        if stripped == alias:
             return False
         pos -= 1
     return True
@@ -101,23 +105,23 @@ def generate_start_spaces(line, clean):
 
 def add_variant_doc_alias(content, clean, enums, is_in_enum, ffi_first, current_info):
     parts = clean.split(" => ")
-    a = parts[0].split("::")[1].strip()
-    b = parts[1].split("::")[1].split("(")[0].split(",")[0].strip()
+    part_a = parts[0].split("::")[1].strip()
+    part_b = parts[1].split("::")[1].split("(")[0].split(",")[0].strip()
     if ffi_first:
         if "ffi::" not in parts[0]:
             return 0
-        ffi_variant = a
-        variant = b
+        ffi_variant = part_a
+        variant = part_b
     else:
         if "ffi::" not in parts[1]:
             return 0
-        ffi_variant = b
-        variant = a
+        ffi_variant = part_b
+        variant = part_a
     variant_pos = find_variant_in_enum(content, enums.get(is_in_enum), variant)
     if variant_pos is None:
-        print("Cannot find `{}` in enum `{}`, ignoring it...".format(variant, is_in_enum))
+        print(f"Cannot find `{variant}` in enum `{is_in_enum}`, ignoring it...")
         return 0
-    alias = '#[doc(alias = "{}")]'.format(ffi_variant)
+    alias = f'#[doc(alias = "{ffi_variant}")]'
     if current_info["ignore_next"] is False and need_doc_alias(content, variant_pos, alias):
         spaces = generate_start_spaces(content[variant_pos], content[variant_pos].strip())
         content.insert(variant_pos, spaces + alias)
@@ -150,12 +154,12 @@ def add_doc_alias_if_needed(content, start, alias, traits, enums, structs, curre
 
 
 def add_parts(path):
-    print("=> Updating '{}'".format(path))
+    print(f"=> Updating '{path}'")
     try:
-        with open(path, 'r') as f:
-            content = f.read().split('\n')
-    except Exception as e:
-        print("Failed to open `{}`: {}".format(path, e))
+        with open(path, 'r', encoding='UTF-8') as file:
+            content = file.read().split('\n')
+    except Exception as err:
+        print(f"Failed to open `{path}`: {err}")
         return (0, 1)
 
     # The key is the trait name, the value is its line number.
@@ -183,7 +187,8 @@ def add_parts(path):
             if clean == "// checker-ignore-item":
                 current_info["ignore_next"] = True
             else:
-                print("[{}:{}] Found unknown `checker` command: `{}`".format(path, current_info["pos"], clean[3:]))
+                print(f'[{path}:{current_info["pos"]}] Found unknown `checker` \
+                    command: `{clean[3:]}`')
                 errors += 1
                 break
         elif not clean.startswith("pub fn") and not clean.startswith("fn") and " => " not in clean:
@@ -213,7 +218,8 @@ def add_parts(path):
                     is_in_trait = trait_name
                 elif ty_name in enums:
                     is_in_enum = ty_name
-                # Trying to get the doc alias from the `impl From<PdfMetadata> for ffi::cairo_pdf_metadata_t`
+                # Trying to get the doc alias from the
+                # `impl From<PdfMetadata> for ffi::cairo_pdf_metadata_t`
                 if trait_name == "From" and ty_name.startswith("ffi::"):
                     impl_for = parts[0].split("From<")[-1].split(">")[0].strip()
                     tmp = None
@@ -222,9 +228,10 @@ def add_parts(path):
                             tmp = kind
                             break
                     if tmp is not None:
-                        alias = '#[doc(alias = "{}")]'.format(ty_name[len("ffi::"):])
+                        alias = f'#[doc(alias = "{ty_name[len("ffi::"):]}")]'
                         start = kind[impl_for]
-                        add_doc_alias_if_needed(content, start, alias, traits, enums, structs, current_info)
+                        add_doc_alias_if_needed(
+                            content, start, alias, traits, enums, structs, current_info)
                 # This is to try to get the FFI name from the `FromGlib<ffi::whatever>`.
                 elif trait_name.startswith("FromGlib") and "ffi::" in clean:
                     tmp = None
@@ -234,9 +241,10 @@ def add_parts(path):
                             break
                     if tmp is not None:
                         ffi_name = clean.split("ffi::")[1].split(">")[0].split(",")[0].strip()
-                        alias = '#[doc(alias = "{}")]'.format(ffi_name)
+                        alias = f'#[doc(alias = "{ffi_name}")]'
                         start = kind[ty_name]
-                        add_doc_alias_if_needed(content, start, alias, traits, enums, structs, current_info)
+                        add_doc_alias_if_needed(
+                            content, start, alias, traits, enums, structs, current_info)
             # This is needed because we want to put Ext traits doc aliases on the trait methods
             # directly and not on their implementation.
             elif clean.startswith("pub trait") or clean.startswith("trait"):
@@ -244,7 +252,8 @@ def add_parts(path):
                 if name.endswith("Ext") or name.endswith("ExtManual"):
                     traits[name] = current_info["pos"]
                     # Completely skip the trait declaration.
-                    while current_info["pos"] < len(content) and content[current_info["pos"]] != "}":
+                    while (current_info["pos"] < len(content) and
+                            content[current_info["pos"]] != "}"):
                         current_info["pos"] += 1
                     continue
             elif clean.startswith("pub enum"):
@@ -256,13 +265,22 @@ def add_parts(path):
                 continue
             elif clean.startswith("pub struct "):
                 if clean.endswith(");") and "ffi::" in clean:
-                    # This is newtype like "pub struct Quark(ffi::GQuark);". We want to extract the ffi
-                    # type and add it as a doc alias.
-                    name = clean.split("ffi::")[1].split(");")[0].split(">")[0].split(",")[0].strip()
-                    alias = '#[doc(alias = "{}")]'.format(name)
-                    add_doc_alias_if_needed(content, current_info["pos"], alias, traits, enums, structs, current_info)
+                    # This is newtype like "pub struct Quark(ffi::GQuark);". We want to extract the
+                    # ffi type and add it as a doc alias.
+                    name = (clean.split("ffi::")[1]
+                        .split(");")[0]
+                        .split(">")[0]
+                        .split(",")[0]
+                        .strip())
+                    alias = f'#[doc(alias = "{name}")]'
+                    add_doc_alias_if_needed(
+                        content, current_info["pos"], alias, traits, enums, structs, current_info)
                 else:
-                    name = clean.split(' struct ')[1].split('<')[0].split(':')[0].split('{')[0].strip()
+                    name = (clean.split(' struct ')[1]
+                            .split('<')[0]
+                            .split(':')[0]
+                            .split('{')[0]
+                            .strip())
                     structs[name] = current_info["pos"]
                     is_in_struct = generate_start_spaces(content[current_info["pos"]], clean) + '}'
             elif is_in_struct is not None and clean.startswith("const "):
@@ -270,8 +288,9 @@ def add_parts(path):
                 ffi_name = clean.split(" = ")[-1].split(";")[0].split(" ")[0]
                 if "ffi::" in ffi_name:
                     ffi_name = ffi_name.split("ffi::")[-1].strip()
-                    alias = '#[doc(alias = "{}")]'.format(ffi_name)
-                    add_doc_alias_if_needed(content, current_info["pos"], alias, traits, enums, structs, current_info)
+                    alias = f'#[doc(alias = "{ffi_name}")]'
+                    add_doc_alias_if_needed(
+                        content, current_info["pos"], alias, traits, enums, structs, current_info)
             current_info["pos"] += 1
             continue
         elif clean.endswith(';'): # very likely a trait method declaration.
@@ -291,7 +310,9 @@ def add_parts(path):
         if is_in_trait is not None:
             trait_method_pos = find_method_in_trait(content, traits.get(is_in_trait), fn_name)
             if trait_method_pos is None:
-                print("Cannot find `{}` in trait `{}`, putting doc aliases on implementation".format(fn_name, is_in_trait))
+                print(
+                    f"Cannot find `{fn_name}` in trait `{is_in_trait}`, putting doc aliases on \
+                      implementation")
             else:
                 start = trait_method_pos
                 need_pos_update = True
@@ -307,8 +328,10 @@ def add_parts(path):
                     # Function/method part
                     # FIXME: might be nice to maybe add a configuration file for such cases...
                     if (sys_name != "gtk_is_initialized" or fn_name != "init"):
-                        alias = '#[doc(alias = "{}")]'.format(sys_name)
-                        if current_info["ignore_next"] is False and need_doc_alias(content, start, alias) and fn_name in sys_name:
+                        alias = f'#[doc(alias = "{sys_name}")]'
+                        if (current_info["ignore_next"] is False and
+                                need_doc_alias(content, start, alias) and
+                                fn_name in sys_name):
                             content.insert(start, spaces + alias)
                             added += 1
                             start += 1
@@ -319,10 +342,12 @@ def add_parts(path):
                     tmp = 0
                     if clean.startswith("ffi::"):
                         # This is for the "form": "ffi::whatever => Self::Whatever"
-                        tmp = add_variant_doc_alias(content, clean, enums, is_in_enum, True, current_info)
+                        tmp = add_variant_doc_alias(
+                            content, clean, enums, is_in_enum, True, current_info)
                     elif clean.startswith("Self::") or clean.startswith(is_in_enum + "::"):
                         # This is for the "form": "Self::Whatever => ffi::whatever"
-                        tmp = add_variant_doc_alias(content, clean, enums, is_in_enum, False, current_info)
+                        tmp = add_variant_doc_alias(
+                            content, clean, enums, is_in_enum, False, current_info)
                     added += tmp
                     current_info["pos"] += tmp
             current_info["pos"] += 1
@@ -333,8 +358,8 @@ def add_parts(path):
 
     # No need to re-write the file if nothing was changed or if an error occurred.
     if errors == 0 and len(content) != original_len:
-        with open(path, 'w') as f:
-            f.write('\n'.join(content))
+        with open(path, 'w', encoding='UTF-8') as file:
+            file.write('\n'.join(content))
     return (len(content) - original_len, errors)
 
 
@@ -365,23 +390,23 @@ def main():
         print("No folder given as argument, updating current `src` (if any)")
         doc_alias_added = run_dirs("src")
     else:
-        for x in sys.argv[1:]:
-            if os.path.isdir(x):
-                print("> Going into folder `{}`...".format(x))
-                ret = run_dirs(x)
+        for entry in sys.argv[1:]:
+            if os.path.isdir(entry):
+                print(f"> Going into folder `{entry}`...")
+                ret = run_dirs(entry)
                 doc_alias_added += ret[0]
                 errors += ret[1]
                 if errors == 0:
                     print("< Done!")
             else:
-                ret = add_parts(x)
+                ret = add_parts(entry)
                 doc_alias_added += ret[0]
                 errors += ret[1]
             if errors > 0:
                 print("An error occurred, aborting...")
     print("")
     if errors == 0:
-        print("Added {} doc aliases.".format(doc_alias_added))
+        print(f"Added {doc_alias_added} doc aliases.")
     return 0
 
 
